777b7d05830cc02c63b2dada2d32deed29e5ea2a
Sukhvir Singh <129220391+Sukhvir-Grewal@users.noreply.github.com>
2023-12-03 23:42:30 -0500

    Version 2.0 adding more dialogs

--- storage/dialogsArray.js
+++ storage/dialogsArray.js
@@ -3,11 +3,14 @@ const dialogsArray = {
         script1: [
             "Hey there! I'm Sukhvir, just like the domain says. Great to meet you, mysterious visitor!",
             "Step right up for a peek into my world!",
+            "Every click is a new adventure. Ready to start yours?",
+            "Peek behind the curtain and see what I've been up to. Spoiler alert: It's exciting stuff!",
         ],
     },
     about: {
         script1: [
             "Apologies, but around here, it's always raining cats and dogs!",
+            "Curious about the person behind the screen? You've come to the right place for stories and more!",
             "By the way, dive in here to uncover all my secrets!",
         ],
         finish: "And there you have it, the complete 411 on me!",
@@ -17,7 +20,8 @@ const dialogsArray = {
     skill: {
         script1: [
             "That's all the wisdom I've got in my pocket for now...",
-            "abc",
+            "However, like a magician, I'm always adding new tricks to my repertoire.",
+            "Remember, every master was once a beginner.",
         ],
         lastVisit: "So, that's my knowledge shared – you're in the loop now!",
         howToOperate: "Give a click and discover just how much I know!",
@@ -26,6 +30,7 @@ const dialogsArray = {
     project: {
         script1: [
             "Check out these handmade masterpieces, no AI magic, just pure brilliance!",
+            "My projects are my dreams woven into reality.",
             "Keen to read what I've written? Prepare for a rollercoaster of words and wit!",
         ],
         lastVisit:
@@ -36,14 +41,12 @@ const dialogsArray = {
     contact: {
         script1: [
             "Now you've got the scoop on me, it's your turn – drop me a line!",
-            "beeju",
+            "Got questions, ideas, or just want to chat? I'm all ears – let's connect!",
         ],
         lastVisit:
             "Already dropped me a line? That's awesome – and if not, don't be shy to reach out!",
-        loading:
-            "Sending...",
-        sent:
-            "Got your message, and I'll reply within a day. My lonely heart can't wait much longer!",
+        loading: "Sending...",
+        sent: "Got your message, and I'll reply within a day. My lonely heart can't wait much longer!",
     },
 };
 
