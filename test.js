1816bde352a393c89f2c9e4734c8433116ea754c
Sukhvir Singh <129220391+Sukhvir-Grewal@users.noreply.github.com>
2023-12-19 20:49:00 -0500

    version 2.0 fixing buling stuff

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
-const test = {
-    a: {
-        script: ["1", "2", "3"],
-        main: "Hello"
-    }
-}
+Running build in Washington, D.C., USA (East) – iad1
+Cloning github.com/Sukhvir-Grewal/portfolio (Branch: master, Commit: 2f8ab5f)
+Skipping build cache, deployment was triggered without cache.
+Cloning completed: 2.071s
+Running "vercel build"
+Vercel CLI 32.7.1
+Installing dependencies...
+npm ERR! code 1
+npm ERR! path /vercel/path0/node_modules/@fortawesome/fontawesome-free
+npm ERR! command failed
+npm ERR! command sh -c node attribution.js
+npm ERR! node: /lib64/libm.so.6: version `GLIBC_2.27' not found (required by node)
+npm ERR! node: /lib64/libc.so.6: version `GLIBC_2.27' not found (required by node)
+npm ERR! node: /lib64/libc.so.6: version `GLIBC_2.28' not found (required by node)
 
-console.log(test['a']['script'])
+npm ERR! A complete log of this run can be found in: /vercel/.npm/_logs/2023-12-20T00_02_27_497Z-debug-0.log
+Error: Command "npm install" exited with 1
